40f91e93987a4d5ddf4940adbc387988ce8e1a6e
Jikr33 <munkhuusuld@gmail.com>
2026-07-29 22:52:40 +0900

    fisrt commits

--- supabase.ts
+++ supabase.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/types";
 
-const SUPABASE_URL =
-  process.env.SUPABASE_URL || "https://cksxnfghcqgvodzqlnzx.supabase.co";
-const SUPABASE_ANON_KEY =
-  process.env.SUPABASE_ANON_KEY || "sb_secret_EODhAKG1PVNOqxA-k-Vbvw_lWciciFs";
+const SUPABASE_URL = process.env.SUPABASE_URL || "";
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 
